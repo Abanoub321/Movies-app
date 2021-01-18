c0b854385f7aa1d49c7edefacd14f775cf13fa1d
Abanoub321 <abanoubmilad12@gmail.com>
2021-01-18 22:47:41 +0200

    feat: menu drawer

--- src/Navigation/DrawerNavigator.js
+++ src/Navigation/DrawerNavigator.js
@@ -1,14 +1,44 @@
 import React from "react";
-
+import { View, TouchableOpacity, Image } from 'react-native';
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import {TrendingStackNavigator} from './StackNavigator';
+import { TrendingStackNavigator } from './StackNavigator';
+import CustomSidebarMenu from './CostomSideBarMenu';
 
 const Drawer = createDrawerNavigator();
 
+export const NavigationDrawerStructure = (props) => {
+  //Structure for the navigatin Drawer
+  const toggleDrawer = () => {
+    //Props to open/close the drawer
+    props.navigationProps.toggleDrawer();
+  };
+
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <TouchableOpacity onPress={toggleDrawer}>
+        {/*Donute Button Image */}
+        <Image
+          source={{
+            uri:
+              'https://raw.githubusercontent.com/AboutReact/sampleresource/master/drawerWhite.png',
+          }}
+          style={{ width: 25, height: 25, marginLeft: 5 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      drawerContentOptions={{
+        activeTintColor: '#e91e63',
+        itemStyle: { marginVertical: 5 },
+      }}
+      drawerContent={(props) => <CustomSidebarMenu {...props} />}>
+
       <Drawer.Screen name="Trending" component={TrendingStackNavigator} />
     </Drawer.Navigator>
   );
--- src/Navigation/StackNavigator.js
+++ src/Navigation/StackNavigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import {NavigationDrawerStructure} from './DrawerNavigator';
 import TrendingScreenComponent from "../Screens/TrendingScreen";
 import MovieScreen from '../Screens/SingleMovieScreen';
 import TvScreen from '../Screens/SingleTvScreen';
@@ -10,20 +11,34 @@ const Stack = createStackNavigator();
 
 const screenOptionStyle = {
   headerStyle: {
-    backgroundColor: "#9AC4F8",
+    backgroundColor: "#f4511e",
+
   },
   headerTintColor: "white",
   headerBackTitle: "Back",
-  
 };
 
-const TrendingStackNavigator = () => {
+const TrendingStackNavigator = ({navigation}) => {
   return (
     <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen name="Trending Today" component={TrendingScreenComponent} />
-      <Stack.Screen name="Movie" component={MovieScreen}  />
-      <Stack.Screen name="Series" component={TvScreen}  />
-      <Stack.Screen name="Season" component={SeasonScreen} />
+      <Stack.Screen name="Trending Today" component={TrendingScreenComponent} options={{
+          
+          headerLeft: () => (
+            <NavigationDrawerStructure
+              navigationProps={navigation}
+            />
+          ),
+          headerStyle: {
+            backgroundColor: '#f4511e', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}/>
+      <Stack.Screen name="Movie" component={MovieScreen} />
+      <Stack.Screen name="Series" component={TvScreen} />
+      <Stack.Screen name="Season" component={SeasonScreen}  />
       <Stack.Screen name="Episode" component={EpisodeScreen}  />
       <Stack.Screen name="Person" component={PersonScreen} />
     </Stack.Navigator>
